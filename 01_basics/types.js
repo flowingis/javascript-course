38ec37211c26de8b6d561cefde7037c60aff92d6
francesco-strazzullo <me@francescostrazzullo.info>
2016-05-12 14:39:28 +0200

    Added types example

---
+++ 01_basics/types.js
@@ -0,0 +1,17 @@
+var a;
+console.log("value of " + a + ": " + typeof a);
+
+a = "hello world";
+console.log("value of " + a + ": " + typeof a);
+
+a = 42;
+console.log("value of " + a + ": " + typeof a);
+
+a = true;
+console.log("value of " + a + ": " + typeof a);
+
+a = undefined;
+console.log("value of " + a + ": " + typeof a);
+
+a = { b: "c" };
+console.log("value of " + a + ": " + typeof a);

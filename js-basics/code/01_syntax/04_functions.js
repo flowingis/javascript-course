9f45502e3484f55be05a131dd4c7289e70fd657e
lorenzo.pomili <lorenzo.pomili85@gmail.com>
2016-06-09 10:05:55 +0200

    added some example of functions

--- js-basics/code/01_syntax/04_functions.js
+++ js-basics/code/01_syntax/04_functions.js
@@ -37,3 +37,51 @@ function printResult2(param){
 
 printResult2(now);
 printResult2("The End");
+
+//Function can be property of object
+var myObject = {
+	boolProp:true,
+	numberProp:42,
+	stringProp:"myString",
+	arrayProp: [1,2,3],
+	funProp: function(param){ 
+		console.log("I'm printing my param: "+param); 
+	},
+	mySubObject: {
+		boolProp:true,
+		numberProp:42,
+		stringProp:"myString",
+		arrayProp: [1,2,3],
+		funProp: function(param){ 
+			console.log("I'm printing my param: "+param); 
+		}	
+	}
+};
+
+myObject.funProp();
+myObject.funProp("Here I am!");
+myObject.funProp(myObject.numberProp);
+
+myObject.mySubObject.funProp();
+myObject.mySubObject.funProp("Here I am!");
+myObject.mySubObject.funProp(myObject.mySubObject.numberProp);
+
+//Fuction can take function as param
+var myModifier = function(element){
+	return element + " is modified";
+};
+
+var mySecondModifier = function(element){
+	return element + 1;
+};
+
+var myArray = [10, 20, 0, 4, 8];
+
+var printModifiedArray = function(arr, elementModifier){
+	for (var i = 0; i < arr.length; i++) {
+		console.log("Log at Index("+i+"): "+elementModifier(arr[i]));
+	}
+};
+
+printModifiedArray(myArray, myModifier);
+printModifiedArray(myArray, mySecondModifier);

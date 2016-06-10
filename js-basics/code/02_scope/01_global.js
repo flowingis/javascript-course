cffa9113b8f5280a0f7acd68d82cbdb2795b9a9e
francesco-strazzullo <me@francescostrazzullo.info>
2016-06-10 16:37:33 +0200

    added another variable to first scope example

--- js-basics/code/02_scope/01_global.js
+++ js-basics/code/02_scope/01_global.js
@@ -1,18 +1,14 @@
 a = 1;
-var b = 1;
 
 console.log(a);
 console.log(global.a);
-console.log(b);
-console.log(global.b);
 
 function dummyFunction(){
     a++;
+    b = a * a;
 };
 
 dummyFunction();
 
-console.log(a);
 console.log(global.a);
-console.log(b);
 console.log(global.b);

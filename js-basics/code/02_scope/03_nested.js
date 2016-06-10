1b8b89e8c20d19859920aa333d2ac754d93f8344
francesco-strazzullo <me@francescostrazzullo.info>
2016-06-10 17:00:11 +0200

    added example for nested scope

---
+++ js-basics/code/02_scope/03_nested.js
@@ -0,0 +1,22 @@
+"use strict"
+
+var a = 1;
+
+function addToA(b){
+    a = a + b;
+    return a;
+};
+
+addToA(1);
+
+console.log(a);
+
+function addToLocalA(b) {
+    var a = 0;
+    a = a + b;
+    return a;
+};
+
+a = addToLocalA(4);
+
+console.log(a);

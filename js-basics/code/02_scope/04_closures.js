bf8095f6df670858a38d18d512ec4bb8fc7017ed
francesco-strazzullo <me@francescostrazzullo.info>
2016-06-10 17:07:22 +0200

    Added closures example

---
+++ js-basics/code/02_scope/04_closures.js
@@ -0,0 +1,20 @@
+"use strict"
+
+var addFunctionFactory = function(initialValue){
+    var a = initialValue || 0;
+    var add = function(b){
+        a = a + b;
+        console.log(a);
+        return a;
+    };
+
+    return add;
+};
+
+var firstFunction = addFunctionFactory(0);
+var secondFunction = addFunctionFactory(2);
+
+firstFunction(2);
+firstFunction(4);
+secondFunction(3);
+secondFunction(5);

5009c65a418593f259f9f645cbf1eaa0652a458e
francesco-strazzullo <me@francescostrazzullo.info>
2016-06-10 17:36:54 +0200

    Added hoisting example

---
+++ js-basics/code/02_scope/03_hoisting.js
@@ -0,0 +1,20 @@
+a = 1;
+var a;
+console.log(a);
+
+console.log('---');
+
+var b;
+b = 2;
+console.log(b);
+
+console.log('---');
+
+console.log(c);
+var c = 2;
+
+console.log('---');
+
+var d;
+console.log(d);
+d = 2;

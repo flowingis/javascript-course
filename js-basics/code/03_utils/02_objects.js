1f313051ad7dd2c0c5f2069e59a3a225c17f13f3
francesco-strazzullo <me@francescostrazzullo.info>
2016-07-28 17:24:01 +0200

    added objects lodash examples

---
+++ js-basics/code/03_utils/02_objects.js
@@ -0,0 +1,61 @@
+var _ = require('../../../node_modules/lodash');
+
+var print = function(data){
+    console.log(JSON.stringify(data, undefined, 2) + '\n');
+};
+
+var user = {
+    id:6,
+    name:'Solid Snake',
+    sex:'M'
+};
+
+print(user);
+
+//Extend
+console.log('Extend');
+console.log('................\n');
+
+var otherData = {
+    name:'Jack',
+    married:false,
+    birthday:'01/01/1970'
+};
+
+var exUser = _.extend({},user,otherData);
+
+print(exUser);
+
+//Defaults
+console.log('Defaults');
+console.log('................\n');
+
+exUser = _.defaults({},user,otherData);
+
+print(exUser);
+
+//Keys
+console.log('Keys');
+console.log('................\n');
+
+console.log(_.keys(user));
+
+//Values
+console.log('Values');
+console.log('................\n');
+
+console.log(_.values(user));
+
+//Pick
+console.log('Pick');
+console.log('................\n');
+
+var onlyName = _.pick(user,'name');
+console.log(onlyName);
+
+//Omit
+console.log('Omit');
+console.log('................\n');
+
+var withoutName = _.omit(user,'name');
+console.log(withoutName);

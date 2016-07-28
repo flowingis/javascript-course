95983de31178b3db95d2dd866b840c43fc05686c
francesco-strazzullo <me@francescostrazzullo.info>
2016-07-28 16:10:51 +0200

    Added lodash first example

---
+++ js-basics/code/03_utils/01_arrays.js
@@ -0,0 +1,104 @@
+var _ = require('../../../node_modules/lodash');
+
+var print = function(data){
+    console.log(JSON.stringify(data, undefined, 2) + '\n');
+};
+
+var users = [
+    {
+        id:6,
+        name:'Solid Snake',
+        sex:'M'
+    },
+    {
+        id:4,
+        name:'Meryl Silverburgh',
+        sex:'F'
+    },
+    {
+        id:9,
+        name:'Liquid Snake',
+        sex:'M'
+    },
+    {
+        id:11,
+        name:'Frank Jaeger',
+        sex:'M'
+    },
+    {
+        id:14,
+        name:'Revolver Ocelot',
+        sex:'M'
+    },
+    {
+        id:18,
+        name:'Sniper Wolf',
+        sex:'F'
+    }
+];
+
+print(users);
+
+//Each
+console.log('Each');
+console.log('................\n');
+
+_.each(users,function(user,index){
+    console.log(index, user.name);
+});
+
+//Map
+console.log('Map');
+console.log('................\n');
+
+var newUsers = _.map(users,function(user,index){
+   return {
+       id:index,
+       name:user.name.toLowerCase()
+   };
+});
+
+print(newUsers);
+
+//Reduce
+console.log('Reduce');
+console.log('................\n');
+
+var veryLongName = _.reduce(users,function (name,user) {
+    return name + user.name + " / ";
+},"Very Long name - ");
+
+console.log(veryLongName);
+
+//OrderBy
+console.log('OrderBy');
+console.log('................\n');
+var orderedUsers = _.orderBy(users,['sex','-name']);
+
+print(orderedUsers);
+
+
+//Filter
+console.log('Filter');
+console.log('................\n');
+
+var females = _.filter(users,{sex:"F"});
+
+print(females);
+
+var highIds = _.filter(users,function (user) {
+   return user.id > 10;
+});
+
+print(highIds);
+
+//Flatten
+console.log('Flatten');
+console.log('................\n');
+console.log(_.flatten([
+    1,
+    [
+        2,
+        3
+    ]
+]));

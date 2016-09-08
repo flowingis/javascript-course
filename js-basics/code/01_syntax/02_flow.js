5d60f3296b6f3b290463dda94d800fc7cd9f850f
francesco-strazzullo <me@francescostrazzullo.info>
2016-09-08 16:05:06 +0200

    cosmetics

--- js-basics/code/01_syntax/02_flow.js
+++ js-basics/code/01_syntax/02_flow.js
@@ -1,14 +1,16 @@
 var x = 0;
 
-//While
+console.log('While');
+console.log('................\n');
+
 while (x < 10) {
     console.log(x);
     x++;
 }
 
-console.log("---");
+console.log('\nDo While');
+console.log('................\n');
 
-//Do While
 x = 20;
 
 do {
@@ -16,25 +18,25 @@ do {
     x++;
 }while (x < 10);
 
-console.log("---");
+console.log('\nFor');
+console.log('................\n');
 
-//For
 for(x = 0;x < 10;x++){
     console.log(x);
 }
 
-console.log("---");
+console.log('\nIf');
+console.log('................\n');
 
-//If
 x = true;
 
 if(x){
     console.log('Here');
 }
 
-console.log("---");
+console.log('\nIf/Else');
+console.log('................\n');
 
-//If Else
 x = false;
 
 if(x){
@@ -43,7 +45,35 @@ if(x){
     console.log('There');
 }
 
-console.log("---");
+console.log('\nIf/Else if');
+console.log('................\n');
+
+x = 3;
+
+if(x == 1){
+    console.log('Here');
+}else if(x == 2){
+    console.log('There');
+}else{
+    console.log('Far away');
+}
+
+console.log('\nSwitch');
+console.log('................\n');
+
+switch(x) {
+    case 1:
+        console.log('Here');
+        break;
+    case 2:
+        console.log('There');
+        break;
+    default:
+        console.log('Far away');
+}
+
+console.log('\nAnd/Or');
+console.log('................\n');
 
 //And / Or
 
@@ -59,34 +89,9 @@ if(true || false){
     console.log('true || false is false');
 }
 
-//If Else If
-x = 3;
+console.log('\nTry/Catch');
+console.log('................\n');
 
-if(x == 1){
-    console.log('Here');
-}else if(x == 2){
-    console.log('There');
-}else{
-    console.log('Far away');
-}
-
-console.log("---");
-
-//Switch
-switch(x) {
-    case 1:
-        console.log('Here');
-        break;
-    case 2:
-        console.log('There');
-        break;
-    default:
-        console.log('Far away');
-}
-
-console.log("---");
-
-//Try/Catch
 try{
     throw new Error('Oh My God! We are doomed!');
 }catch (e){

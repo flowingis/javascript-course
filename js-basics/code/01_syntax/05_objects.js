fee2a41f81c37eac3cf31a40a5ad2f076942c1c7
francesco-strazzullo <me@francescostrazzullo.info>
2016-09-08 16:18:11 +0200

    cosmetics

--- js-basics/code/01_syntax/05_objects.js
+++ js-basics/code/01_syntax/05_objects.js
@@ -1,20 +1,29 @@
-var key = 'third';
+console.log('Object literals');
+console.log('................\n');
 
 var obj = {
     first:'first',
     second:2
 };
 
+console.log("obj = ", JSON.stringify(obj, undefined, 2) + '\n');
+
+console.log('\nNesting');
+console.log('................\n');
+
 var child = {
     first:1
 };
-
-console.log("obj = ", JSON.stringify(obj, undefined, 2) + '\n');
 
 obj.third = child;
 obj.third.first = 2;
 
 console.log("obj = ", JSON.stringify(obj, undefined, 2) + '\n');
+
+console.log('\nDynamic keys');
+console.log('................\n');
+
+var key = 'third';
 
 obj[key] = undefined;
 

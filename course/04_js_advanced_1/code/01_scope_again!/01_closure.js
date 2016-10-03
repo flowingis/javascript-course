63472b98df7c288193eba6cdf4392214b8681167
francesco-strazzullo <me@francescostrazzullo.info>
2016-10-03 16:35:39 +0200

    added comments to scope example

--- course/04_js_advanced_1/code/01_scope_again!/01_closure.js
+++ course/04_js_advanced_1/code/01_scope_again!/01_closure.js
@@ -2,7 +2,16 @@ var accumulatorFactory = function (initialValue) {
     var value = initialValue || 0;
 
     var add = function (i) {
+        /*
+            in questo caso il valore di value, non viene trovato
+            all'interno della funzione 'add' e quindi salgo di un livello
+            arrivando a 'accumulatorFactory'
+         */
         value += parseInt(i,10) || 0;
+        /*
+            ritorno l'istanza che verrà ritornata verso l'esterno per applicare la Fluent interface,
+            sarebbe l'equivalente di un "return this"
+         */
         return accumulatorInstance;
     };
 

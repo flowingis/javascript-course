e448ea5d0f541ad44137d054f2dfc1d35c1f3e35
francesco-strazzullo <me@francescostrazzullo.info>
2016-10-05 12:50:11 +0200

    build exam

--- course/04_js_advanced_1/code/02_this_and_new/08_exam.js
+++ course/04_js_advanced_1/code/02_this_and_new/08_exam.js
@@ -1,15 +1,20 @@
-var Person = function(name,surname,age){
+var Person = function(name,surname,age,sex){
     this.name = name;
     this.surname = surname;
     this.age = age;
+    this.sex = sex;
+
+    this.isAdult = function(){
+        return this.age >= 18;
+    };
 
     this.getFullname = function () {
         return this.name + " " + this.surname;
     }
 };
 
-var MaleDecorator = function(name, surname, age){
-    Person.call(this,name,surname,age);
+var Male = function(name, surname, age){
+    Person.call(this,name, surname, age, 'M');
 
     var originalGetFullname = this.getFullname.bind(this);
 
@@ -18,6 +23,57 @@ var MaleDecorator = function(name, surname, age){
     };
 };
 
-var person = new MaleDecorator('Franceco','Strazzullo',30);
+var Female = function(name, surname, age){
+    Person.call(this,name, surname, age, 'F');
 
-console.log(person.getFullname());
+    var originalGetFullname = this.getFullname.bind(this);
+
+    this.getFullname = function(){
+        return "Ms " + originalGetFullname();
+    };
+};
+
+var PersonBuilder = function(){
+    this.person = false;
+
+    this.createPerson = function(){
+        this.person = {};
+        return this;
+    };
+
+    this.setSex = function(sex){
+        this.sex = sex;
+        return this;
+    };
+
+    this.setName = function(name){
+        this.name = name;
+        return this;
+    };
+
+    this.setSurname = function(surname){
+        this.surname = surname;
+        return this;
+    };
+
+    this.setAge = function(age){
+        this.age = age;
+        return this;
+    };
+
+    this.getPerson = function () {
+        var Constructor = this.sex === 'M' ? Male : Female;
+        return new Constructor(this.name,this.surname,this.age);
+    };
+};
+
+var me = new Male('Franceco','Strazzullo',30);
+var myGirlfriend = new Female('Lucia','Morbidelli',29);
+
+var personBuilder = new PersonBuilder();
+
+var anotherMe = personBuilder.createPerson().setSex('M').setName('Francesco').setSurname('Strazzullo').setAge(30).getPerson();
+
+console.log(me.getFullname());
+console.log(myGirlfriend.getFullname());
+console.log(anotherMe.getFullname());

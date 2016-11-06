948d28d69e6db042f7a88f8d3a9488acbc9913e1
lorenzo.pomili <lorenzo.pomili85@gmail.com>
2016-11-06 13:52:47 +0100

    refactored and added comment on 01_timeout_interval

--- course/05_advanced-js/code/02_async/01_timeout_interval.js
+++ course/05_advanced-js/code/02_async/01_timeout_interval.js
@@ -1,10 +1,11 @@
 "use strict"
 
-const serviceResponse = {
+let serviceResponse = {
     staticResponse: {},
     dinamicResponse: {}
 };
 
+// after 3 seconds staticResponse is initiallized
 setTimeout(function(){
     serviceResponse.staticResponse = {
         "a":1,
@@ -13,16 +14,24 @@ setTimeout(function(){
     };
 }, 3000);
 
+// each 1.5 seconds dinamicResponse.couter will update by 1, or initiallized to 0 at first iteration
 var updateCounter = setInterval(function(){
-    const couter = serviceResponse.dinamicResponse.couter;
+    let couter = serviceResponse.dinamicResponse.couter;
     (couter || couter === 0) ? serviceResponse.dinamicResponse.couter++ : serviceResponse.dinamicResponse.couter = 0;
 }, 1500);
 
+let secondCounter = 1;
+
+// each second staticResponse and counter is printed
 var printStatus = setInterval(function(){
+    console.log("Sec: "+secondCounter);
     console.log(serviceResponse.staticResponse);
     console.log(serviceResponse.dinamicResponse.couter);
+    console.log("------------------------------");
+    secondCounter++;
 }, 1000);
 
+// after 10 seconds both interval is removed
 setTimeout(function(){
     clearInterval(updateCounter);
     clearInterval(printStatus);

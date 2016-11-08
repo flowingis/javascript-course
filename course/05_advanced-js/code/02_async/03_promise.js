906e792eff370c6d21b207110da4836c2b205780
lorenzo.pomili <lorenzo.pomili85@gmail.com>
2016-11-08 13:01:58 +0100

    refactoring of 03_promise

--- course/05_advanced-js/code/02_async/03_promise.js
+++ course/05_advanced-js/code/02_async/03_promise.js
@@ -5,11 +5,6 @@ var counterForCallback = {
     val2: 0
 };
 
-var counterForPromise = {
-    val1: 0,
-    val2: 0
-};
-
 var increaseVal1ByOneWithCallback = function(counter, callback){
     setTimeout(function(){
         counter.val1++;
@@ -24,56 +19,174 @@ var increaseVal2ByValueWithCallback = function(counter, val, callback){
     }, 1500);
 };
 
-var increaseVal1ByOneWithPromise = function(counter){
+
+var getCounterPlusValue = function(counter, value){
     return new Promise(function(resolve, reject){
         setTimeout(function(){
-            counter.val1++;
-                resolve();
-        }, 3000);
+            var sum = counter + value;
+            if(sum <= 100){
+                resolve(sum);
+            }else{
+                reject({
+                    counter: counter,
+                    value: value,
+                    sum: sum
+                });
+            }
+        }, 1500);
+    });  
+};
+
+// in promise chain with no error all call is execute
+var chainWithoutError = function(callback){
+    console.log("CHAIN WITHOUT ERROR\n");
+    getCounterPlusValue(0, 1).then(function(counter){
+        console.log("NO ERROR: getCounterPlusValue from 0 with 1 -> "+counter+"\n");
+
+        return getCounterPlusValue(counter, 10);
+    }).then(function(counter){
+        console.log("NO ERROR: getCounterPlusValue from 1 with 10 -> "+counter+"\n");
+
+        return getCounterPlusValue(counter, 10);
+    }).then(function(counter){
+        console.log("NO ERROR: getCounterPlusValue from 11 with 10 -> "+counter+"\n");
+
+        return getCounterPlusValue(counter, 10);
+    }).then(function(counter){
+        console.log("NO ERROR: getCounterPlusValue from 21 with 10 -> "+counter+"\n");
+    }).catch(function(e){
+        console.log("will no pass here: NO ERROR");
+    }).then(function(){
+        console.log("finally anyway here\n");
+        console.log("/CHAIN WITHOUT ERROR\n");
+        callback();
     });
 };
 
-var increaseVal2ByValueWithPromise = function(counter, val){
-    return new Promise(function(resolve, reject){
-        setTimeout(function(){
-            counter.val2+=val;
-                if(counter.val2 >= 10){
-                    resolve();
-                }else{
-                    reject();
-                }
-        }, 3000);
+// in promise chain with managed error all call is execute the .then of the one who go in error will use second function to manage error
+var chainWithManagedError = function(callback){
+    console.log("CHAIN WITH MANAGED ERROR\n");
+    getCounterPlusValue(0, 1).then(function(counter){
+        console.log("getCounterPlusValue from 0 with 1 -> "+counter+"\n");
+
+        return getCounterPlusValue(counter, 10);
+    }).then(function(counter){
+        console.log("getCounterPlusValue from 1 with 10 -> "+counter+"\n");
+
+        return getCounterPlusValue(counter, 20);
+    }).then(function(counter){
+        console.log("getCounterPlusValue from 21 with 20 -> "+counter+"\n");
+
+        return getCounterPlusValue(counter, 80);
+    }).then(
+        function(counter){
+            console.log("getCounterPlusValue from 41 with 80 -> "+counter+"\n");
+            return getCounterPlusValue(counter, 10);
+        }, function(e){
+            console.log(e.counter+" + "+e.value+" = "+e.sum+"\n");
+            Promise.reject();
+        }
+    ).then(function(counter){
+        console.log("getCounterPlusValue from 121 with 10 -> "+counter+"\n");
+    }).catch(function(e){
+        console.log("Managed error is not catched\n"); 
+    }).then(function(){
+        console.log("finally here\n");
+        console.log("/CHAIN WITH MANAGED ERROR\n");
+        callback();
     });
 };
 
-increaseVal1ByOneWithCallback(counterForCallback, function(){
-    console.log(JSON.stringify("Callback -> val1+1: "+JSON.stringify(counterForCallback)));
-    
-    increaseVal2ByValueWithCallback(counterForCallback, 10, function(){
-        console.log(JSON.stringify("Callback -> val2+10: "+JSON.stringify(counterForCallback)));
+// in promise with unmamaged error the chain is stop at the call who return error and result of reject can be catched
+var chainWithUnmanagedError = function(callback){
+    console.log("CHAIN WITH UNMANAGED ERROR\n");
+    getCounterPlusValue(0, 1).then(function(counter){
+        console.log("getCounterPlusValue from 0 with 1 -> "+counter+"\n");
+
+        return getCounterPlusValue(counter, 10);
+    }).then(function(counter){
+        console.log("getCounterPlusValue from 1 with 10 -> "+counter+"\n");
+
+        return getCounterPlusValue(counter, 20);
+    }).then(function(counter){
+        console.log("getCounterPlusValue from 21 with 20 -> "+counter+"\n");
+        // this call will cause error
+        return getCounterPlusValue(counter, 80);
+    }).then(function(counter){
+        console.log("getCounterPlusValue from 41 with 80 -> "+counter+"\n");
+
+        return getCounterPlusValue(counter, 10);
+    }).then(function(counter){
+        console.log("getCounterPlusValue from 121 with 10 -> "+counter+"\n");
+    }).catch(function(e){
+        console.log("Unmanaged error is catched: "+JSON.stringify(e)+"\n"); 
+    }).then(function(){
+        console.log("finally here\n");
+        console.log("/CHAIN WITH UNMANAGED ERROR\n");
+        callback();
+    });
+};
+
+// this is an example of pyramid of doom with callbacks
+var callbackChain = function(callback){
+    console.log("CALLBACK CHAIN\n");
+    increaseVal1ByOneWithCallback(counterForCallback, function(){
+        console.log(JSON.stringify("Callback -> val1+1: "+JSON.stringify(counterForCallback))+"\n");
         
-        increaseVal2ByValueWithCallback(counterForCallback, 11, function(){
-            console.log(JSON.stringify("Callback -> val2+11: "+JSON.stringify(counterForCallback)));
+        increaseVal2ByValueWithCallback(counterForCallback, 10, function(){
+            console.log(JSON.stringify("Callback -> val2+10: "+JSON.stringify(counterForCallback))+"\n");
             
-            increaseVal2ByValueWithCallback(counterForCallback, 100, function(){
-                console.log(JSON.stringify("Callback -> val2+100: "+JSON.stringify(counterForCallback)));
+            increaseVal2ByValueWithCallback(counterForCallback, 11, function(){
+                console.log(JSON.stringify("Callback -> val2+11: "+JSON.stringify(counterForCallback))+"\n");
+                
+                increaseVal2ByValueWithCallback(counterForCallback, 100, function(){
+                    console.log(JSON.stringify("Callback -> val2+100: "+JSON.stringify(counterForCallback))+"\n");
+                    console.log("/CALLBACK CHAIN\n");
+                    callback();
+                });
             });
         });
     });
-});
+};
 
-increaseVal1ByOneWithPromise(counterForPromise).then(function(){
-    console.log(JSON.stringify("Promise -> val1+1: "+JSON.stringify(counterForPromise)));
-    
-    return increaseVal2ByValueWithPromise(counterForPromise, 10);
-}).then(function(){
-    console.log(JSON.stringify("Promise -> val2+10: "+JSON.stringify(counterForPromise)));
+var standAloneCall1 = function(n){
+    return new Promise(function(resolve, reject){
+        setTimeout(function(){
+            resolve("Call1: "+n);
+        }, 1000);
+    });
+};
 
-    return increaseVal2ByValueWithPromise(counterForPromise, 11);
-}).then(function(){
-    console.log(JSON.stringify("Promise -> val2+11: "+JSON.stringify(counterForPromise)));
+var standAloneCall2 = function(n){
+    return new Promise(function(resolve, reject){
+        setTimeout(function(){
+            resolve("Call2: "+n);
+        }, 2000);
+    });
+};
 
-    return increaseVal2ByValueWithPromise(counterForPromise, 100);
-}).then(function(){
-    console.log(JSON.stringify("Promise -> val2+100: "+JSON.stringify(counterForPromise)));
+var standAloneCall3 = function(n){
+    return new Promise(function(resolve, reject){
+        setTimeout(function(){
+            resolve("Call3: "+n);
+        }, 1000);
+    });
+};
+
+var promiseAll = function(){
+    console.log("PROMISE ALL\n");
+    Promise.all([standAloneCall1(1), standAloneCall2(2), standAloneCall3(3)]).then(value => { 
+        console.log(value[0]+"\n");
+        console.log(value[1]+"\n");
+        console.log(value[2]+"\n");
+        console.log("/PROMISE ALL\n");
+    });
+};
+
+callbackChain(function(){
+    chainWithManagedError(function(){
+        chainWithUnmanagedError(function(){
+            chainWithoutError(promiseAll);  
+        });
+    });
 });

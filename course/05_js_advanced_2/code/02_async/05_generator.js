c38f696eb2a15d6a33d927ba0b4c3a908575aea2
lorenzo.pomili <lorenzo.pomili85@gmail.com>
2016-11-10 11:06:10 +0100

    added generator example

---
+++ course/05_js_advanced_2/code/02_async/05_generator.js
@@ -0,0 +1,34 @@
+"use strict"
+
+
+var infiniteSequentialUnitGenerator = function* (){
+    var counter = 0
+    while(true){
+        try{
+            yield counter++;
+        }catch(e){
+            console.log("Error: "+e);
+        }
+    }
+};
+
+var unit = infiniteSequentialUnitGenerator();
+var unit2 = infiniteSequentialUnitGenerator();
+
+console.log("UNIT GENERATOR");
+console.log(unit.next());
+console.log(unit.next());
+console.log(unit.next());
+// return set value to passed value and stop generator
+console.log(unit.return(10));
+console.log(unit.next());
+console.log("/UNIT GENERATOR");
+
+console.log("UNIT2 GENERATOR");
+console.log(unit2.next());
+console.log(unit2.next());
+console.log(unit2.next());
+// throw use error catching of generator but don't stop it
+console.log(unit2.throw("my error"));
+console.log(unit2.next());
+console.log("/UNIT2 GENERATOR");

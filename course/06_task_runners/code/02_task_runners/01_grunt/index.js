26ea49472b27b94bd7f435813d2e055816ff970d
francesco-strazzullo <me@francescostrazzullo.info>
2016-11-23 13:58:11 +0100

    cosmetics

--- course/06_task_runners/code/02_task_runners/01_grunt/index.js
+++ course/06_task_runners/code/02_task_runners/01_grunt/index.js
@@ -1,8 +1,16 @@
 $(document).ready(function () {
-    $('button').click(function () {
-        $('#todo').append("<ul>" + $("input[name=task]").val() + " <a href='#' class='close' aria-hidden='true'>&times;</a></ul>");
+
+    var todoContainer =  $('#todo');
+    var input = $("input[name=task]");
+
+    $('button[role=add]').click(function () {
+        var li = "<li>" + input.val() + " <a href='#' class='close' aria-hidden='true'>&times;</a></li>"
+        todoContainer.append(li);
+        input.val("");
+        input.focus();
     });
+
     $("body").on('click', '#todo a', function () {
-        $(this).closest("ul").remove();
+        $(this).closest("li").remove();
     });
 });

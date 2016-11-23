69c34a4aedc9a9e91c278ff2c27ab9a4d0998668
francesco-strazzullo <me@francescostrazzullo.info>
2016-11-23 13:49:54 +0100

    added build with grunt

--- course/06_task_runners/code/02_task_runners/01_grunt/Gruntfile.js
+++ course/06_task_runners/code/02_task_runners/01_grunt/Gruntfile.js
@@ -1,8 +1,28 @@
 module.exports = function(grunt) {
 
-    grunt.loadNpmTasks('grunt-contrib-connect');
-
     grunt.initConfig({
+        concat: {
+            js: {
+                src: [
+                    'node_modules/jquery/dist/jquery.js',
+                    'node_modules/bootstrap/dist/js/bootstrap.js',
+                    'index.js'
+                ],
+                dest: 'build/app.js'
+            },
+            css: {
+                src: [
+                    'node_modules/bootstrap/dist/css/bootstrap.css',
+                    'app.css'
+                ],
+                dest: 'build/style.css'
+            }
+        },
+        clean: {
+            build: [
+                'build'
+            ]
+        },
         connect: {
             server: {
                 options: {
@@ -12,6 +32,42 @@ module.exports = function(grunt) {
                     livereload: true
                 }
             }
-        }
+        },
+        uglify: {
+            js: {
+                files: {
+                    'build/app.min.js': ['build/app.js']
+                }
+            }
+        },
+        cssmin: {
+           target: {
+                files: {
+                    'build/style.min.css': ['build/style.css']
+                }
+            }
+        },
+        processhtml: {
+            dist: {
+                files: {
+                    'build/index.html': ['index.html']
+                }
+            }
+        },
     });
+
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-processhtml');
+    grunt.loadNpmTasks('grunt-contrib-connect');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+
+    grunt.registerTask('build', [
+        'clean:build',
+        'concat',
+        'uglify',
+        'cssmin',
+        'processhtml:dist'
+    ]);
 };

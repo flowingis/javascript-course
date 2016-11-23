f0cff639f6a3a4dd51819d0815cdd1393aaf4eee
francesco-strazzullo <me@francescostrazzullo.info>
2016-11-23 11:51:05 +0100

    added rest server example

---
+++ course/06_task_runners/code/01_node/06_rest_server.js
@@ -0,0 +1,48 @@
+var os = require('os');
+var express = require('express');
+var bodyParser = require('body-parser');
+var _ = require('lodash');
+
+/*
+    Creiamo un'isntanza di server semplicemente inizializzando express
+ */
+var server = express();
+
+/*
+    Aggiungiamo un body parser per leggere i dati dalle post
+ */
+server.use(bodyParser.text());
+
+/*
+    Tramite i metodi get, post, delete, etc possiamo definire le rotte alla quali il nostro server risponde
+ */
+server.get('/status', function(req, res) {
+    var status = {
+        platform:os.platform(),
+        uptime: os.uptime(),
+        username:  os.userInfo().username
+    };
+
+    /*
+        Notate che la chiamata rimane pending finché non viene invocato res.send
+     */
+    setTimeout(function () {
+        res.send(status)
+    },1000);
+
+});
+
+server.post('/kebab', function(req, res){
+    /*
+        Una volta installato il body parser, possiamo leggere il valore dalla request
+     */
+    res.send(_.kebabCase(req.body));
+});
+
+
+/*
+    Facciamo infine partire il server sulla porta 3000
+ */
+server.listen(3000, function () {
+    console.log('Example server listening on port 3000!')
+});

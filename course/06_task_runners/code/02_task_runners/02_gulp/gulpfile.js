956ba2ac10bc4be675f093a1a2350fc619cc7d41
nandoluc <d.luconi@e-xtrategy.net>
2016-11-29 14:38:52 +0100

    node and task runners: added livereload to gulp

--- course/06_task_runners/code/02_task_runners/02_gulp/gulpfile.js
+++ course/06_task_runners/code/02_task_runners/02_gulp/gulpfile.js
@@ -4,7 +4,10 @@ var useref = require('gulp-useref');
 var cleanDest = require('gulp-clean-dest');
 
 gulp.task('connect', function() {
-    connect.server();
+    connect.server({
+        root: './',
+        livereload: true
+    });
 });
 
 gulp.task('build', function () {
@@ -13,3 +16,15 @@ gulp.task('build', function () {
         .pipe(useref())
         .pipe(gulp.dest('build'));
 });
+
+
+gulp.task('html', function () {
+  gulp.src('./*.html')
+    .pipe(connect.reload());
+});
+ 
+gulp.task('watch', function () {
+  gulp.watch(['./*.html'], ['html']);
+});
+ 
+gulp.task('default', ['connect', 'watch']);

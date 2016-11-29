e58dab69953f5148fea5cf4a63a8644ed99424d0
nandoluc <d.luconi@e-xtrategy.net>
2016-11-29 11:53:04 +0100

    node and task runners: rest server

--- course/06_task_runners/code/01_node/06_rest_server.js
+++ course/06_task_runners/code/01_node/06_rest_server.js
@@ -10,42 +10,42 @@ var express = require('express');
 var bodyParser = require('body-parser');
 
 /*
-    Creiamo un'istanza di server semplicemente inizializzando express
- */
+Creiamo un'istanza di server semplicemente inizializzando express
+*/
 var server = express();
 
 /*
-    Aggiungiamo un body parser per leggere i dati dalle post
- */
+Aggiungiamo un body parser per leggere i dati dalle post
+*/
 
 // var jsonParser = bodyParser.json()
 var textParser = bodyParser.text();
 
 /*
-    Tramite i metodi get, post, delete, etc possiamo definire le rotte alla quali il nostro server risponde
- */
+Tramite i metodi get, post, delete, etc possiamo definire le rotte alla quali il nostro server risponde
+*/
 server.get('/status', function(req, res) {
     var status = {
         platform:os.platform(),
         uptime: os.uptime(),
         username:  os.userInfo().username
     };
-
+    
     /*
-        Notate che la chiamata rimane pending finché non viene invocato res.send
-     */
+    Notate che la chiamata rimane pending finché non viene invocato res.send
+    */
     setTimeout(function () {
         res.send(status);
     },1000);
-
+    
 });
 
 server.post('/kebab', textParser, function(req, res){
     /*
-        Una volta installato il body parser, possiamo leggere il valore dalla request
-     */
-    if(!req.body){
-        res.status(400).send();
+    Una volta installato il body parser, possiamo leggere il valore dalla request
+    */
+    if(_.isEmpty(req.body)){
+        res.status(400).send('bad request');
     }else{
         res.status(200).send(_.kebabCase(req.body));
     }
@@ -53,8 +53,8 @@ server.post('/kebab', textParser, function(req, res){
 
 
 /*
-    Facciamo infine partire il server sulla porta 3000
- */
+Facciamo infine partire il server sulla porta 3000
+*/
 server.listen(3000, function () {
     console.log('Example server listening on port 3000!')
 });

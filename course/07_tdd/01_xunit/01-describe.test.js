8a875b9b22f7df7546a9f75a7d1827d73da00937
francesco-strazzullo <me@francescostrazzullo.info>
2016-12-05 12:54:37 +0100

    added comments

--- course/07_tdd/01_xunit/01-describe.test.js
+++ course/07_tdd/01_xunit/01-describe.test.js
@@ -1,3 +1,12 @@
+/*
+    describe e it servono per creare la struttura del report dei vostri test. In questo caso l'outcome è il seguente
+        outer
+            inner
+                first
+            inner again
+                second
+
+ */
 describe('outer', () => {
     describe('inner', () => {
         it('first', () => {

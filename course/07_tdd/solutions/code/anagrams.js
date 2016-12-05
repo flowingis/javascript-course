3f29a47635abce32f477af236da561fc4675c629
francesco-strazzullo <me@francescostrazzullo.info>
2016-12-05 23:40:02 +0100

    finishing tests

--- course/07_tdd/solutions/code/anagrams.js
+++ course/07_tdd/solutions/code/anagrams.js
@@ -1,3 +1,5 @@
+const fs = require('fs');
+const path = require('path');
 const _ = require('lodash');
 
 const findPermutations = (string) => {
@@ -37,9 +39,66 @@ const permutations = (word) => {
         return [chars[1]+chars[0]];
     }
 
-    return _.without(findPermutations(word),word).sort();
+    return _.uniq(_.without(findPermutations(word),word)).sort();
+};
+
+const splitter = (phrase,opts = {}) => {
+    let words = phrase.split(opts.separator || " ");
+
+    if(opts.minLength){
+        words = words.filter(word => word.length >= opts.minLength);
+    }
+
+    if(opts.maxLength){
+        words = words.filter(word => word.length <= opts.maxLength);
+    }
+
+    return words;
+};
+
+const wordValidator = (word,dictionary, blacklist = []) => {
+    return !blacklist.includes(word) && dictionary.includes(word);
+};
+
+const anagrams = (phrase, opts = {}) => {
+    if(!phrase){
+        throw new Error('please provide a phrase')
+    }
+
+    let words;
+
+    if(_.isArray(phrase)){
+        words = phrase;
+    }else{
+        if(typeof phrase !== 'string'){
+            throw new Error('please provide a string')
+        }
+
+        words = splitter(phrase,opts)
+    }
+
+    const dictionary = JSON.parse(fs.readFileSync(path.join(__dirname,'..','..','words.json')));
+
+    const toReturn = {};
+
+    words.forEach(word => {
+        const permutations = permutations(word);
+
+        let validPermutations = permutations.filter(permutation => {
+            return wordValidator(permutation,dictionary,opts.blacklist);
+        });
+
+        if(validPermutations.length){
+            toReturn[word] = validPermutations;
+        }
+    });
+
+    return toReturn;
 };
 
 module.exports = {
-    permutations
+    permutations,
+    splitter,
+    wordValidator,
+    anagrams
 };

5f891b673ca37bb85bf628f7e7c9d8b6463cbfdb
Francesco Strazzullo <francesco.strazzullo86@gmail.com>
2017-01-02 14:24:37 +0100

    created search-bar component

--- course/08_angular2/code/src/app/app.module.ts
+++ course/08_angular2/code/src/app/app.module.ts
@@ -2,22 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import { PanelModule } from 'primeng/primeng';
+import { PanelModule, InputTextModule, ButtonModule } from 'primeng/primeng';
 
 import { AppComponent } from './app.component';
 import { WeatherService } from './services/weather.service';
 import { WeatherIconComponent } from './components/weather-icon/weather-icon.component';
+import { SearchBarComponent } from './components/search-bar/search-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    WeatherIconComponent
+    WeatherIconComponent,
+    SearchBarComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
-    PanelModule
+    PanelModule,
+    InputTextModule,
+    ButtonModule
   ],
   providers: [
     WeatherService
---
+++ course/08_angular2/code/src/app/components/search-bar/search-bar.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'search-bar',
+  templateUrl: './search-bar.component.html'
+})
+export class SearchBarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

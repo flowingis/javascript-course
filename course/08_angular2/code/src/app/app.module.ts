f0da2e90854273a9fbf59af99d02357c67f0b969
Francesco Strazzullo <francesco.strazzullo86@gmail.com>
2017-01-02 09:18:23 +0100

    empty angular2 project with primeng

---
+++ course/08_angular2/code/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { PanelModule } from 'primeng/primeng';
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    PanelModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }

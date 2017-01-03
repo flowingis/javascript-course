38e42727af8cc8cb6ee3106041b6dda2732f65bd
Francesco Strazzullo <francesco.strazzullo86@gmail.com>
2017-01-03 09:06:58 +0100

    Guarding new comment route

--- course/08_angular2/code/src/app/pages/new-comment/new-comment.component.ts
+++ course/08_angular2/code/src/app/pages/new-comment/new-comment.component.ts
@@ -1,14 +1,23 @@
+import { LastWeatherInfoService } from './../../services/last-weather-info.service';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-new-comment',
   templateUrl: './new-comment.component.html'
 })
 export class NewCommentComponent implements OnInit {
+  private lastWeatherInfo;
 
-  constructor() { }
+  constructor(
+    private lastWeatherInfoService: LastWeatherInfoService,
+    private router: Router) { }
 
   ngOnInit() {
+    this.lastWeatherInfo = this.lastWeatherInfoService.get();
+    if(!this.lastWeatherInfo){
+        this.router.navigate(['/']);
+    }
   }
 
 }

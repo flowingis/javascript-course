42c372d2bd75c4b34df408e58e07069f31089161
Francesco Strazzullo <francesco.strazzullo86@gmail.com>
2017-01-03 14:48:17 +0100

    Using reactive form to gather data

--- course/08_angular2/code/src/app/pages/new-comment/new-comment.component.ts
+++ course/08_angular2/code/src/app/pages/new-comment/new-comment.component.ts
@@ -3,7 +3,8 @@ import { CommentsService } from './../../services/comments.service';
 import { LastWeatherInfoService } from './../../services/last-weather-info.service';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators, ValidatorFn, AbstractControl } from '@angular/forms';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-new-comment',
@@ -20,19 +21,25 @@ export class NewCommentComponent implements OnInit {
     private lastWeatherInfoService: LastWeatherInfoService,
     private commentsService: CommentsService,
     private router: Router,
-    private formBuilder: FormBuilder) { 
-      this.reactions = [
-        'Happy',
-        'Sad',
-        'Angry'
-      ];
+    private formBuilder: FormBuilder) {
+    this.reactions = [
+      'Happy',
+      'Sad',
+      'Angry'
+    ];
 
-      this.form = this.formBuilder.group({
-          reaction: [this.reactions[0]],
-          message: ['', Validators.required],
-          date: [new Date(), Validators.required]
-      });
+    const dateValidator = (control: FormControl) => {
+      if (!moment(control.value).isSameOrAfter(new Date(), 'day')) {
+        return { invalidDate: true };
+      }
     }
+
+    this.form = this.formBuilder.group({
+      reaction: [this.reactions[0]],
+      message: ['', Validators.required],
+      date: [new Date(), Validators.compose([Validators.required, dateValidator])]
+    });
+  }
 
   ngOnInit() {
     this.lastWeatherInfo = this.lastWeatherInfoService.get();
@@ -42,11 +49,13 @@ export class NewCommentComponent implements OnInit {
   }
 
   onSave() {
-    if (this.message) {
+    if (this.form.valid) {
       const comment = {
-        message: this.message,
+        message: this.form.controls.message.value,
         city: this.lastWeatherInfo.name,
-        username: this.currentUserService.get()
+        username: this.currentUserService.get(),
+        date: moment(this.form.controls.date.value).format(),
+        reaction: this.form.controls.reaction.value
       };
 
       this.commentsService.add(comment).subscribe((r) => {

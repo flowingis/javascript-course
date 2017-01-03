6714606b338a9a34272fb5e23e6bd08442a5f9db
Francesco Strazzullo <francesco.strazzullo86@gmail.com>
2017-01-03 08:47:46 +0100

    Moved component to home page

--- course/08_angular2/code/src/app/app.component.ts
+++ course/08_angular2/code/src/app/app.component.ts
@@ -1,10 +1,4 @@
 import { Component } from '@angular/core';
-import { WeatherService } from './services/weather.service';
-import { CommentsService } from './services/comments.service';
-import { Observable } from 'rxjs/Observable';
-
-import 'rxjs/add/operator/zip';
-
 
 @Component({
   selector: 'app-root',
@@ -12,21 +6,5 @@ import 'rxjs/add/operator/zip';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  currentWeahterInfo;
-  loading=false;
-
-  constructor(private weatherService: WeatherService, private commentsService: CommentsService){
-   
-  }
-
-  onSearch(searchString) {
-    this.loading = true;
-
-    this.weatherService.current(searchString).zip(this.commentsService.list(searchString)).subscribe(results => {
-      const currentWeahterInfo = results[0];
-      currentWeahterInfo.comments = results[1];
-      this.currentWeahterInfo = currentWeahterInfo;
-      this.loading = false;
-    })
-  }
+  
 }
--- course/08_angular2/code/src/app/pages/home/home.component.ts
+++ course/08_angular2/code/src/app/pages/home/home.component.ts
@@ -1,14 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { WeatherService } from '../../services/weather.service';
+import { CommentsService } from '../../services/comments.service';
+import { Observable } from 'rxjs/Observable';
+
+import 'rxjs/add/operator/zip';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html'
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent {
+currentWeahterInfo;
+  loading=false;
 
-  constructor() { }
+  constructor(private weatherService: WeatherService, private commentsService: CommentsService){ }
 
-  ngOnInit() {
+  onSearch(searchString) {
+    this.loading = true;
+
+    this.weatherService.current(searchString).zip(this.commentsService.list(searchString)).subscribe(results => {
+      const currentWeahterInfo = results[0];
+      currentWeahterInfo.comments = results[1];
+      this.currentWeahterInfo = currentWeahterInfo;
+      this.loading = false;
+    })
   }
-
 }

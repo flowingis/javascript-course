2eb05445b6946bfe944fe17e248911b31f0797d3
Francesco Strazzullo <francesco.strazzullo86@gmail.com>
2017-02-13 10:23:57 +0100

    react hello world

--- course/10_react/src/index.js
+++ course/10_react/src/index.js
@@ -1,1 +1,7 @@
-console.log('Daje')
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+ReactDOM.render(
+  <h1>Hello, world!</h1>,
+  document.getElementById('root')
+)
--- course/10_react/webpack.config.js
+++ course/10_react/webpack.config.js
@@ -3,5 +3,22 @@ module.exports = {
   output: {
     filename: 'bundle.js',
     path: __dirname
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        use: [
+          {
+            loader: 'babel-loader',
+            query: {
+              presets: ['es2015', 'react']
+            }
+          }
+
+        ],
+        exclude: 'node_modules'
+      }
+    ]
   }
 }

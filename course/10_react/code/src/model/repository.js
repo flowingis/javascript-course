ada085f752365d5ce61be7ddaa382f952131bedb
Francesco Strazzullo <francesco.strazzullo86@gmail.com>
2017-02-17 18:28:38 +0100

    added repository

---
+++ course/10_react/code/src/model/repository.js
@@ -0,0 +1,21 @@
+const todoList = []
+
+const list = () => {
+  return Object.freeze(todoList)
+}
+
+const store = todo => {
+  todoList.push(todo)
+  return list()
+}
+
+const remove = index => {
+  todoList.splice(index, 1)
+  return list()
+}
+
+export default {
+  list,
+  store,
+  remove
+}

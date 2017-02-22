f23b839989604d7d7558a55627cce2c4619e7365
Francesco Strazzullo <francesco.strazzullo86@gmail.com>
2017-02-22 19:41:36 +0100

    Added auto run

--- course/10_react/code/src/index.js
+++ course/10_react/code/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { autorun } from 'mobx'
 
 import App from './components/App'
 import AppState from './model/AppState'
@@ -7,6 +8,10 @@ import repositoryFactory from './model/repository'
 
 const state = new AppState()
 const repository = repositoryFactory(state)
+
+autorun(() => {
+  console.log('Total number of todos: ' + state.todoNumber)
+})
 
 ReactDOM.render(
   <App state={state} repository={repository} />,

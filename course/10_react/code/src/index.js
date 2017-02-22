0e8b618b2163bec65f7340eb5103140948fcd5f4
Francesco Strazzullo <francesco.strazzullo86@gmail.com>
2017-02-22 17:40:31 +0100

    added redux logger

--- course/10_react/code/src/index.js
+++ course/10_react/code/src/index.js
@@ -1,12 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
-import { createStore } from 'redux'
+import { createStore, compose, applyMiddleware } from 'redux'
+import createLogger from 'redux-logger'
 
 import App from './components/App'
 import reducer from './redux/reducer'
 
-const store = createStore(reducer)
+const logger = createLogger({
+  collapsed: true
+})
+
+const store = createStore(reducer,
+  compose(
+    applyMiddleware(logger),
+    window.devToolsExtension ? window.devToolsExtension() : f => f
+  )
+)
 
 ReactDOM.render(
   <Provider store={store}>
